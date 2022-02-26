19d7fd7d412d34de393ad02cc1bfab2a754701a2
alusch7 <luschera@mcmaster.ca>
2022-02-26 03:17:13 -0500

    Finished! (I think)

--- WinOrLoseRockPaperScissors/ContentView.swift
+++ WinOrLoseRockPaperScissors/ContentView.swift
@@ -24,10 +24,10 @@ extension View {
 struct ContentView: View {
     @State private var gameOver = false
     @State private var WorL = Bool.random()
-    @State private var computer = Int.random(in: 0..<2)
+    @State private var computer = 0
     @State private var score = 0
     @State private var rounds = 0
-    @State private var outcomes = "Hello"
+    @State private var outcomes = ""
     @State private var backgrounds = ""
     @State private var options = ["👊", "🤚", "✌️"]
     var body: some View {
@@ -36,18 +36,21 @@ struct ContentView: View {
                 Color(.gray)
                     .ignoresSafeArea()
             } else if backgrounds == "G" {
-                Color(.green)
+                Color(red: 0.13, green: 0.60, blue: 0.02)
+                    .ignoresSafeArea()
             } else if backgrounds == "R" {
-                Color(.red)
-            } else {
-                Color(.yellow)
+                Color(red: 0.79, green: 0.23, blue: 0.13)
+                    .ignoresSafeArea()
+            } else if backgrounds == "Y" {
+                Color(red: 0.84, green: 0.73, blue: 0.11)
+                    .ignoresSafeArea()
             }
-            
             VStack {
                 Text("Score: \(score)")
                     .foregroundColor(.white)
                     .font(.title2.bold())
                     .padding(.trailing)
+                    .padding(.bottom, 0.1)
                     .frame(maxWidth: .infinity, alignment: .topTrailing)
                 if WorL == true {
                     Text("Try to Win")
@@ -57,6 +60,7 @@ struct ContentView: View {
                         .WorLTitle()
                 }
                 Spacer()
+                    .frame(maxHeight:50)
                 if rounds == 0 {
                     VStack {
                         Text("Choose an Emoji to Begin")
@@ -70,30 +74,66 @@ struct ContentView: View {
                     }
                 } else {
                     Text("You \(outcomes)")
+                        .foregroundColor(.white)
+                        .font(.title.bold())
+                    Spacer()
+                    Text("\(options[computer])")
+                        .font(.system(size:150))
+                        .padding()
+                        .shadow(color: .black, radius: 1, x: 2, y: 2)
                 }
                 Spacer()
                     .frame(maxHeight: 200)
-                HStack {
-                    ForEach(0..<3) { number in
-                        Button(options[number]) {
-                            emojiTapped(number, computer, WorL)
-                            WorL.toggle()
-                            computer = Int.random(in: 0..<2)
-                            rounds += 1
-                        } .font(.system(size:60))
-                            .padding()
-                            .shadow(color: .black, radius: 1, x: 2, y: 2)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25)
+                        .frame(maxWidth: .infinity, maxHeight: 120)
+                        .padding(.horizontal, 20)
+                        .foregroundStyle(.ultraThinMaterial)
+                    HStack {
+                        ForEach(0..<3) { number in
+                            Button(options[number]) {
+                                rounds += 1
+                                if rounds != 10 {
+                                    computer = Int.random(in: 0..<3)
+                                    emojiTapped(number, computer, WorL)
+                                    WorL.toggle()
+                                } else {
+                                    emojiTapped(number, computer, WorL)
+                                    gameOver = true
+                                }
+                            } .font(.system(size:60))
+                                .padding()
+                                .shadow(color: .black, radius: 1, x: 2, y: 2)
+                        }
                     }
                 }
             }
+        }.alert("Game Over", isPresented: $gameOver) {
+            Button("Restart", role: .cancel) {
+                rounds = 0
+                score = 0
+                outcomes = ""
+            }
+        } message: {
+            Text("Great job! You won \(score) rounds!")
         }
     }
     
     func emojiTapped(_ number: Int, _ computer: Int, _ WorL: Bool) {
-        if WorL == true {
-            if number == computer {
-                outcomes = "Tied!"
-            } else if (number == 1 && computer == 0) || (number == 2 && computer == 1) || (number == 0 && computer == 2) {
+        if number == computer {
+            outcomes = "Tied!"
+            backgrounds = "Y"
+        } else if WorL == true {
+            if (number == 1 && computer == 0) || (number == 2 && computer == 1) || (number == 0 && computer == 2) {
+                outcomes = "Won!"
+                score += 1
+                backgrounds = "G"
+            } else {
+                outcomes = "Lost!"
+                backgrounds = "R"
+            }
+        } else {
+            if (number == 0 && computer == 1) || (number == 1 && computer == 2) || (number == 2 && computer == 0) {
                 outcomes = "Won!"
                 score += 1
                 backgrounds = "G"
@@ -103,10 +143,6 @@ struct ContentView: View {
             }
         }
     }
-    
-    /*func emojiTapped(_ number: Int, _ computer: Int, _ WorL: Bool) {
-        
-    }*/
 }
 
 struct ContentView_Previews: PreviewProvider {
