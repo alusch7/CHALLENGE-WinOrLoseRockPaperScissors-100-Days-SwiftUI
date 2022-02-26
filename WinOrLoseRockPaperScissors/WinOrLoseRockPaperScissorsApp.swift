9057d4f94ec6f6d6e966b7b0866307f45665b140
alusch7 <luschera@mcmaster.ca>
2022-02-25 23:13:48 -0500

    Initial Commit

---
+++ WinOrLoseRockPaperScissors/WinOrLoseRockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinOrLoseRockPaperScissorsApp.swift
+//  WinOrLoseRockPaperScissors
+//
+//  Created by Alden Luscher on 2022-02-25.
+//
+
+import SwiftUI
+
+@main
+struct WinOrLoseRockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
